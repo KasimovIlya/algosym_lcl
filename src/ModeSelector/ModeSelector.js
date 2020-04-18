24fd81a4aac6e6e7fa91e045d67f796059dcf673
Ilya <KIM9999999@mail.ru>
2020-04-18 23:15:37 +0300

    Update ModeSelector.js

--- src/ModeSelector/ModeSelector.js
+++ src/ModeSelector/ModeSelector.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 export default props =>{
-//     const smallUrl = `http://localhost:8000/api/getTop`;
-    const smallUrl = ``http://www.mocky.io/v2/5e9b06743300009432bf1681`;
-    
+    const smallUrl = process.env.REACT_APP_API_URL;
     return (
         <div style={{display:'flex', justifyContent:'center', padding: '50px 0'}}>
             <button onClick={()=>props.onSelect(smallUrl)} className="btn btn-success">Начать</button>
